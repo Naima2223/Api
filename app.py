e4d32e7415a01f15b1661d64634517df976066f5
Naima2223 <135008925+Naima2223@users.noreply.github.com>
2023-06-07 10:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask import Flask
 from flask import request, jsonify
 app = Flask(__name__)
 
-@app.route("/predictTag")
+#@app.route("/predictTag")
+@app.route("/")
+def hello():
+    return "Hello World!"
+   
 def predictTag():
    # return "Naima: Hello World!"
     json_ = request.json
