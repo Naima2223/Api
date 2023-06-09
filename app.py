4bddb7ecf6a72dd5a01ca964a63625197cfa1e0a
Naima2223 <aitallaounaima@gmail.com>
2023-06-09 11:38:25 +0200

    analyse bug azure 2

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import request, jsonify, render_template
+
 app = Flask(__name__)
 
 # Home page
 @app.route('/') 
 def index():
     return render_template('index.html')
+	
+	
+@app.route('/predictTag', methods=['POST'])
+   
+def predictTag():
+   return render_template('recommendation.html', tags = 'Retour test', questions='test')
 	
 if __name__ == "__main__":
     app.run(debug=True)	
