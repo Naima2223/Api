6a35a6a4fce104a2745a5d9b21fed0bd138ff5be
Naima2223 <aitallaounaima@gmail.com>
2023-06-09 11:10:46 +0200

    Modif

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def predictTag():
 	if question is not None:
 		question 		= str(question)
 		#return jsonify({"prediction": question})
-		#question 		= review_to_tockens(question)
-		#question 		= vectors.transform(question)
-		#output 			= transformer.transform(question)
-		#result 			= models.prediction(model, 0.35, output, target_col)
-		recommendation 	= question #result
-	return render_template('recommendation.html', tags = recommendation)# jsonify({"prediction": question}))
+		question 		= review_to_tockens(question)
+		question 		= vectors.transform(question)
+		output 			= transformer.transform(question)
+		result 			= models.prediction(model, 0.35, output, target_col)
+		recommendation 	= result
+	return render_template('recommendation.html', tags = recommendation, questions=str(request.form['question']))# jsonify({"prediction": question}))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/models.py
+++ models/models.py
@@ -4,6 +4,7 @@ def prediction(model,
                threshold: float,
                text,
                target_col: list):
+	
 
     '''
 
@@ -15,16 +16,20 @@ def prediction(model,
     :param target_col: column to be used. They represent the tags searched
     :return: dictionary containing the different tags suggested
     '''
-
     Y_pred_proba = model.predict_proba(text)
     tmp_results = []
+	
     for n_proba in range(len(Y_pred_proba)):
-
-        for i in range(len(Y_pred_proba[n_proba])):
+        res = []
+        for i in range(len(Y_pred_proba[n_proba])):    
             if Y_pred_proba[n_proba][i][1] > threshold:
-                tmp_results.append(1)
+                res.append(1)
             else:
-                tmp_results.append(0)
+                res.append(0)
+        if 1 in res:
+            tmp_results.append(1)  
+        else:
+            tmp_results.append(0)				
 
     target_col = [x.replace('target_', '') for x in target_col]
 
