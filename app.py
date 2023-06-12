ff2a7fd7036a24f4c0b8b2f9408fc601efae277e
Naima2223 <aitallaounaima@gmail.com>
2023-06-12 12:59:37 +0200

    test complet

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 from flask import Flask
@@ -8,6 +9,14 @@ import sklearn
 from joblib import load
 from models import models
 
+model_path 	= "models/stack_overflow_tag_prediction.joblib"
+target_path = "models/target_col.joblib"
+vectors_path 	= "models/tfidf_vectorizer.joblib"
+transformer_path = "models/normalize.joblib"
+model = load(model_path)
+target_col = load(target_path)
+vectors = load(vectors_path)
+transformer = load(transformer_path)
 
 
 
@@ -17,12 +26,27 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     return render_template('index.html')
-	
-	
+
+
 @app.route('/predictTag', methods=['POST'])
    
 def predictTag():
-   return render_template('recommendation.html', tags = 'Retour test', questions='test')
-	
+   # return "Naima: Hello World!"
+   #json_ = request.json
+   #data = json_
+	question 		= request.form['question']#data['question']  
+	recommendation = ''
+	if question is not None:
+		question 		= str(question)
+		#return jsonify({"prediction": question})
+		question 		= review_to_tockens(question)
+		question 		= vectors.transform(question)
+		output 			= transformer.transform(question)
+		result 			= models.prediction(model, 0.35, output, target_col)
+		recommendation 	= result
+	return render_template('recommendation.html', tags = recommendation, questions=str(request.form['question']))# jsonify({"prediction": question}))
+
 if __name__ == "__main__":
-    app.run(debug=True)	
+    app.run(debug=True)
+	#from waitress import serve
+	#serve(app, host="0.0.0.0", port=8080)
